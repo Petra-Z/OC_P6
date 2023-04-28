2610765310ac6f46abcad95bdd36ab5376ce7b0a
Petra <116842877+Petra-Z@users.noreply.github.com>
2023-04-28 19:16:14 +0300

    fetch dans le index.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,10 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+   async function getPhotographers() {
+      
+         // Recuperation les data du fichier JSON en utilisant "fetch".
+        return fetch('/data/photographers.json')
+         .then(response => response.json())
+        //  .then(data => console.log(data))
+         .catch(error => console.log(error));
     }
 
     async function displayData(photographers) {
@@ -43,4 +24,3 @@
     };
     
     init();
-    
